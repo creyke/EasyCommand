0a876540b6dbdbf7899334fa0eefde5a070ef324
Alex Shannon <alexs1305@gmail.com>
2019-03-25 09:01:18 +0000

    feat(exceptionhandlers): create test specific exception handlers

---
+++ EasyCommand.AspNetCore.Tests/Handler/FailAfterCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EasyCommand.AspNetCore.Tests.Handler
+{
+    class FailAfterCommandHandler : IAsyncAspCommandHandler
+    {
+        public Task AfterExecutionAsync<TResponse>(Type command, TResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BeforeExecutionAsync<TRequest>(Type command, TRequest request)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ EasyCommand.AspNetCore.Tests/Handler/FailBeforeCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EasyCommand.AspNetCore.Tests.Handler
+{
+    class FailBeforeCommandHandler : IAsyncAspCommandHandler
+    {
+        public Task AfterExecutionAsync<TResponse>(Type command, TResponse response)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task BeforeExecutionAsync<TRequest>(Type command, TRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    
+}
